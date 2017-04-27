83cf2486f4c5b32d79e1a3bf06f78b5d34530bc3
Raksha B M <Raksha B M>
2017-04-27 23:24:44 +0530

    merged the file watcher and read file code

--- src/main/java/com/mycompany/cncanalysis/filewatcher/FileWatcher.java
+++ src/main/java/com/mycompany/cncanalysis/filewatcher/FileWatcher.java
@@ -6,6 +6,9 @@
 
 package com.mycompany.cncanalysis.filewatcher;
 import static com.sun.nio.file.ExtendedWatchEventModifier.FILE_TREE;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
@@ -38,9 +41,32 @@ public class FileWatcher {
                 if(e.kind().toString() == "ENTRY_CREATE" || e.kind().toString() == "ENTRY_MODIFY"){
                     path = pTemp.toString()+ "\\" + c;
                     System.out.println(path);
+                    readFile(path);
                 }
             }
             k.reset();
         }
     }
+      
+    public static void readFile(String path){
+                  
+        try
+            {
+//make a 'file' object 
+                File file = new File(path);
+//Get data from this file using a file reader. 
+    FileReader fr = new FileReader(file);
+//To store the contents read via File Reader
+    BufferedReader br = new BufferedReader(fr);                                                 
+//Read br and store a line in 'data', print data
+    String data;
+    while((data=br.readLine( )) != null) 
+{
+    //data = br.readLine( );                                     
+    System.out.println(data);
+}                                
+}
+        catch(IOException e){System.out.println("bad !");
+                }
+      }
 }
