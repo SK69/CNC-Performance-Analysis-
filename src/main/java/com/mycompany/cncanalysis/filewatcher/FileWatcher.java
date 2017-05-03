72f196dd7da36c8f5f49ae6fd6a39ff5cd7e47bb
ShivHkumar <shivkumar030495@gmail.com>
2017-05-04 00:50:02 +0530

    parsing the data using Scanner

--- src/main/java/com/mycompany/cncanalysis/filewatcher/FileWatcher.java
+++ src/main/java/com/mycompany/cncanalysis/filewatcher/FileWatcher.java
@@ -5,7 +5,8 @@
  */
 
 package com.mycompany.cncanalysis.filewatcher;
-import static com.sun.nio.file.ExtendedWatchEventModifier.FILE_TREE;
+
+import  static com.sun.nio.file.ExtendedWatchEventModifier.FILE_TREE;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -18,17 +19,15 @@ import static java.nio.file.StandardWatchEventKinds.*;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.Scanner;
 
-/**
- *
- * @author Raksha
- */
+
 public class FileWatcher {
       public static void main(String[] args) throws Exception
     {
         FileSystem fs = FileSystems.getDefault();
         WatchService ws = fs.newWatchService();
-        Path pTemp = Paths.get("R:\\New Folder");
+        Path pTemp = Paths.get("S:\\tesfile");
         String path;
         pTemp.register(ws, new WatchEvent.Kind[] {ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE}, FILE_TREE);
         while(true)
@@ -41,32 +40,24 @@ public class FileWatcher {
                 if(e.kind().toString() == "ENTRY_CREATE" || e.kind().toString() == "ENTRY_MODIFY"){
                     path = pTemp.toString()+ "\\" + c;
                     System.out.println(path);
-                    readFile(path);
+                    readFile(Paths.get(path));
                 }
             }
             k.reset();
         }
     }
-      
-    public static void readFile(String path){
-                  
-        try
-            {
-//make a 'file' object 
-                File file = new File(path);
-//Get data from this file using a file reader. 
-    FileReader fr = new FileReader(file);
-//To store the contents read via File Reader
-    BufferedReader br = new BufferedReader(fr);                                                 
-//Read br and store a line in 'data', print data
-    String data;
-    while((data=br.readLine( )) != null) 
-{
-    //data = br.readLine( );                                     
-    System.out.println(data);
-}                                
-}
-        catch(IOException e){System.out.println("bad !");
-                }
-      }
+
+    public static void readFile(Path path) throws IOException {
+
+        Scanner scan = new Scanner(path);
+
+        while (scan.hasNext())
+        {
+            String[] tokens = scan.nextLine().split("-");
+            String last  = tokens[tokens.length-1];
+            System.out.println(last);
+
+        }
+
+    }
 }
